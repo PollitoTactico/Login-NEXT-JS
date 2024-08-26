a8483ee188ea1d4dd2a97fe3322556efed2b5997
José Francisco Sánchez Ortega <jose.sanchez.ortega@udla.edu.ec>
2024-08-25 22:21:09 -0500

    first commit

---
+++ marcianos-homosexuales/src/app/page.js
@@ -0,0 +1,20 @@
+import Image from "next/image";
+import { startTransition } from "react";
+
+export default function Home() {
+  return (
+    <div>
+      {/*  IMAGEN */}
+      <div>
+
+      </div>
+
+      {/* LOGIN */}
+      <div>
+        
+      </div>
+
+    </div>
+  );
+}
+
